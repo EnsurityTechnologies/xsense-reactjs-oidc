cfaf2fe5ae8c7f56f338e033832213db216b3fff
Rajasekhar Naidu <rajasekhard2015@outlook.com>
2023-03-22 21:28:20 +0530

    Add project files.

---
+++ src/helpers/Constants.ts
@@ -0,0 +1,8 @@
+export class Constants {
+  public static stsAuthority = 'https://ensurity.xsensezts.com/';
+  public static clientId = 'xsense.public';
+  public static clientRoot = 'http://localhost:3000/';
+  public static clientScope = 'openid profile email api';
+
+  public static apiRoot = 'https://ensurity.xsensezts.com/api/';
+}
